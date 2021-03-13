10ded82056fd17755354b7f38d390e7f0d7232d0
Suneth Perera <sunethjoes@gmail.com>
2021-03-13 12:02:31 +1100

    Excursions client view feed from database

--- resources/js/components/ClientView/Excursions.js
+++ resources/js/components/ClientView/Excursions.js
@@ -1,92 +1,97 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import axios from 'axios';
-import '../../css/Excursion.css';
-class Excursions extends Component{
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { connect } from "react-redux";
+import "../../css/Excursion.css";
+import { listExcursions } from "../actions/ExcursionAction";
 
-    render(){
+class Excursions extends Component {
+    componentDidMount() {
+        this.props.fetchData();
+    }
+
+    renderExcursions() {
+        return this.props.excursions.map(item => (
+            <div className="col-md-4" key={item.excursion_id}>
+                <div className="card mb-3 mt-3 excursion-card-rounded">
+                    <div className="row no-gutters">
+                        <div className="col">
+                            <div className="card-header excursion-header">
+                                <img
+                                    className=""
+                                    width="100%"
+                                    height="100%"
+                                    src={
+                                        process.env.MIX_PUBLIC_IMAGE_URL +
+                                        item.primaryimg
+                                    }
+                                />
+                            </div>
+                            <div className="card-footer excursion-footer">
+                                <p className="p-3 float-left excursion-name">
+                                    {item.name}
+                                </p>
+                                <button className="btn btn-info float-right excursion-btn">
+                                    Explore
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        ));
+    }
+    render() {
         return (
             <>
                 <div className="container mt-5">
                     <div className="row">
                         <div className="col-12">
-                            <div className="section-title h2">
-                                Excursions
-                            </div>
+                            <div className="section-title h2">Excursions</div>
                             <p>
-                                Lorem ipsum dolor sit amet, curabitur nec
-                                lacus pellentesque ut facilisis, lacus iaculis
-                                turpis interdum pede, sapien quis amet vitae,
-                                erat parturient, turpis congue sit. Hac nulla
+                                Lorem ipsum dolor sit amet, curabitur nec lacus
+                                pellentesque ut facilisis, lacus iaculis turpis
+                                interdum pede, sapien quis amet vitae, erat
+                                parturient, turpis congue sit. Hac nulla
                                 phasellus ornare. Volutpat risus ipsum nulla
                                 ducimus erat. Scelerisque eu imperdiet wisi
                                 wisi, sit libero sed ipsum sodales phasellus,
                                 odio vel ac non ac, sodales viverra, metus
                                 volutpat quis rutrum diam ac integer. Posuere
-                                nullam eu vestibulum non nonummy. Metus purus
-                                ac malesuada vitae ut qui, sed rhoncus
-                                nonummy, massa ac urna risus, faucibus aliquam
-                                malesuada fusce gravida urna.
+                                nullam eu vestibulum non nonummy. Metus purus ac
+                                malesuada vitae ut qui, sed rhoncus nonummy,
+                                massa ac urna risus, faucibus aliquam malesuada
+                                fusce gravida urna.
                             </p>
                         </div>
                     </div>
                     <div className="row">
-                        <div className="col-md-4">
-                            <div className="card mb-3 mt-3 excursion-card-rounded">
-                                <div className="row no-gutters">
-                                    <div className="col">
-                                        <div className="card-header excursion-header">
-                                            <img className="" width="100%" height="100%" src={process.env.MIX_PUBLIC_IMAGE_URL + 'colombo_excursion.jpg'}/>
-                                        </div>
-                                        <div className="card-footer excursion-footer">
-                                            <p className="p-3 float-left excursion-name">Colombo city tour</p>
-                                            <button className="btn btn-info float-right excursion-btn">Explore</button>
-                                        </div>
-                                    </div>
-                                </div>
+                        {typeof this.props.excursions !== "undefined"
+                            ? this.renderExcursions()
+                            : ""}
+                        {typeof this.props.excursions !== "undefined" &&
+                        this.props.excursions.length > 3 ? (
+                            <div className="col-12 d-flex justify-content-center">
+                                <button className="btn btn-more px-3 text-black text-nowrap nav-link">
+                                    View More
+                                </button>
                             </div>
-                        </div>
-                        <div className="col-md-4">
-                            <div className="card mb-3 mt-3 excursion-card-rounded">
-                                <div className="row no-gutters">
-                                    <div className="col">
-                                        <div className="card-header excursion-header">
-                                            <img className="" width="100%" height="100%" src={process.env.MIX_PUBLIC_IMAGE_URL + 'galle_excursion.jpg'}/>
-                                        </div>
-                                        <div className="card-footer excursion-footer">
-                                            <p className="p-3 float-left excursion-name">Galle tour</p>
-                                            <button className="btn btn-info float-right excursion-btn">Explore</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-4">
-                            <div className="card mb-3 mt-3 excursion-card-rounded">
-                                <div className="row no-gutters">
-                                    <div className="col">
-                                        <div className="card-header excursion-header">
-                                            <img className="" width="100%" height="100%" src={process.env.MIX_PUBLIC_IMAGE_URL + 'sigiriya_excursion.jpg'}/>
-                                        </div>
-                                        <div className="card-footer excursion-footer">
-                                            <p className="p-3 float-left excursion-name">Sigiriya tour</p>
-                                            <button className="btn btn-info float-right excursion-btn">Explore</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        ) : (
+                            ""
+                        )}
                     </div>
                 </div>
             </>
         );
     }
-
-    async onButtonClick(){
-        let res = await axios.get('/api/packages');
-        let projects = res.data;
-        // console.log(`res ${JSON.stringify(res)}`);
-    }
 }
+const mapStateToProps = state => {
+    const { ExcursionReducer } = state;
+    const { excursions } = ExcursionReducer;
+    return { excursions };
+};
 
-export default Excursions;
+const mapDispatchToProps = dispatch => ({
+    fetchData: () => dispatch(listExcursions())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Excursions);
